17e9bb58ae26efe304b5fe789dc3b6a8b7020014
meton-robean <meton-robean@qq.com>
2019-10-19 20:29:23 +0800

    2019-10-19 [TAG]linked-list

--- 203.remove-linked-list-elements.py
+++ 203.remove-linked-list-elements.py
@@ -4,6 +4,19 @@
 # [203] Remove Linked List Elements
 #
 
+# Category	Difficulty	Likes	Dislikes
+# algorithms	Easy (36.42%)	1007	59
+# Tags
+# Companies
+# Remove all elements from a linked list of integers that have value val.
+
+# Example:
+
+# Input:  1->2->6->3->4->5->6, val = 6
+# Output: 1->2->3->4->5
+
+
+############################
 # @lc code=start
 # Definition for singly-linked list.
 # class ListNode(object):
